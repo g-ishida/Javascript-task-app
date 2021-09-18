418c368298df578997f7375be58bc995ac36ae2a
石田岳 <gakuishida@ishidatakenoMacBook-ea.local>
2021-09-18 17:54:13 +0900

    一応保存

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 'use strict'
 {
+    // IDを取得
+    const all = document.getElementById('all');
+    const doing = document.getElementById('doing');
+    const done = document.getElementById('done');
+    const inputTask = document.getElementById('input-task');
+    const add = document.getElementById('add');
+
+    add.addEventListener('click', () => {
+        let taskName = inputTask.value;
+        console.log(taskName);
+        inputTask.value = ""; //inputの中身を空にする
+    });
     
 }
